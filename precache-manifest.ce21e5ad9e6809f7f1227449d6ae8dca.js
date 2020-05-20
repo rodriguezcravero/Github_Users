ccd026a965ac89d75ae76638b005e4de9e6b3fbc
nachorodriguez2 <nacho_rodriguez85@gmail.com>
2020-05-20 20:18:15 -0300

    Updates

---
+++ precache-manifest.ce21e5ad9e6809f7f1227449d6ae8dca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c291fd6e3ae4d792d726d3c54e697d8",
+    "url": "/Github_Users/index.html"
+  },
+  {
+    "revision": "2c09b18126489c43abd6",
+    "url": "/Github_Users/static/css/main.06a5c9ed.chunk.css"
+  },
+  {
+    "revision": "d7431cc7160aed0164cd",
+    "url": "/Github_Users/static/js/2.88866556.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Github_Users/static/js/2.88866556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c09b18126489c43abd6",
+    "url": "/Github_Users/static/js/main.1ebbbf4d.chunk.js"
+  },
+  {
+    "revision": "e03eb13b6499e5651cb0",
+    "url": "/Github_Users/static/js/runtime-main.624b1bc9.js"
+  }
+]);
